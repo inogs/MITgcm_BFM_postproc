cc1a709e015ecf14a73dff5b7cee46edb65d0a0f
Stefano Piani <spiani@ogs.it>
2024-08-23 21:50:32 +0200

    Added CLI flag  to build_layer_map.py script

--- build_layer_map.py
+++ build_layer_map.py
@@ -1,20 +1,20 @@
 import argparse
-
+from utilities.argparse_types import date_from_str, existing_dir_path, \
+    existing_file_path
 
 
 def argument():
-    parser = argparse.ArgumentParser(description = '''
-    Generates adriatic medeaf maps
-    ''')
-
+    parser = argparse.ArgumentParser(
+        description='Generates adriatic medeaf maps'
+    )
 
     parser.add_argument(   '--outputdir',"-o",
-                                type = str,
+                                type = existing_dir_path,
                                 required = True,
                                 help = '/some/path/')
 
     parser.add_argument(   '--inputdir', '-i',
-                                type = str,
+                                type = existing_dir_path,
                                 default = None,
                                 required = True,
                                 help ='''Directory containg merged outputs of MITgcm
@@ -26,21 +26,28 @@ def argument():
                                 default = 'ave*nc',
                                 help = 'glob search pattern')
 
-    parser.add_argument(   '--maskfile',"-m",
-                                type = str,
+    parser.add_argument(   '--maskfile', "-m",
+                                type = existing_file_path,
                                 default = None,
                                 required = True
                                 )
-    parser.add_argument(   '--plotlistfile',"-p",
-                                type = str,
+    parser.add_argument(   '--plotlistfile', "-p",
+                                type = existing_file_path,
                                 default = None,
                                 required = True
                                 )
-    parser.add_argument(   '--mapconfig',"-f",
-                                type = str,
+    parser.add_argument(   '--mapconfig', "-f",
+                                type = existing_file_path,
                                 default = None,
                                 required = True
                                 )
+    parser.add_argument(   '--ignore-before', "-b",
+                                type = date_from_str,
+                                default = None,
+                                required = False,
+                                help = 'Ignore all the frames before this date'
+                                )
+
     return parser.parse_args()
 
 args = argument()
@@ -48,22 +55,16 @@ args = argument()
 
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from commons.Timelist import TimeList
-from commons import genUserDateList as DL
-import numpy as np
-import numpy.ma as ma
 from commons.mask import Mask
 from utilities.mpi_serial_interface import get_mpi_communicator
 
 from commons.dataextractor import DataExtractor
 from layer_integral.mapbuilder import MapBuilder
-import pylab as pl
 from commons.layer import Layer
 
 from map_gen import Map_object
-
-from datetime import datetime
-from commons.utils import addsep
 
 try:
     import mpi4py
@@ -71,69 +72,66 @@ except:
     pass
 
 comm = get_mpi_communicator()
-rank  = comm.Get_rank()
-nranks =comm.size
+rank = comm.Get_rank()
+nranks = comm.size
 
+map_obj = Map_object.create_from_file(args.mapconfig)
 
+TL = TimeList.fromfilenames(None, args.inputdir, args.pattern)
 
+if args.ignore_before is not None:
+    valid_frames = [f for f in TL.Timelist if f >= args.ignore_before]
+else:
+    valid_frames = [f for f in TL.Timelist]
 
-
-map_obj=Map_object.create_from_file(args.mapconfig)
-
-INPUTDIR=addsep(args.inputdir)
-OUTPUTDIR=addsep(args.outputdir)
-
-
-TL = TimeList.fromfilenames(None, INPUTDIR, args.pattern )
-
-dateformat="%Y%m%d-%H:%M:%S"
-
-
+dateformat = "%Y%m%d-%H:%M:%S"
 
 TheMask = Mask(args.maskfile)
-jpk,jpj,jpi=TheMask.shape
-plotlistfile=args.plotlistfile
+jpk, jpj, jpi = TheMask.shape
+plotlistfile = args.plotlistfile
 
-mb = MapBuilder(plotlistfile,TL,TheMask,OUTPUTDIR)
+mb = MapBuilder(plotlistfile, TL, TheMask, args.outputdir)
 
 for p in mb._MapBuilder__plotlist:
-    var=p.varname
     LAYERLIST = p.layerlist
 
-    ALL_INDEXES = np.arange(TL.nTimes)
-    local_INDEXES = ALL_INDEXES[rank::nranks]
+    # The indices and the dt values of the frames of this process
+    local_frames = tuple(enumerate(valid_frames))[rank::nranks]
 
-    if var == 'Speed':
-        for iFrame_plot in local_INDEXES:
-                dt=TL.Timelist[iFrame_plot]
-                inputfileU = "%save.%s.%s.nc"  %(INPUTDIR,dt.strftime(dateformat),'U')
-                inputfileV = "%save.%s.%s.nc"  %(INPUTDIR,dt.strftime(dateformat),'V')
-                datestr=dt.strftime("%d %h %Y - %H:%M UTC")
-                print("rank %d works on %s" %(rank, inputfileU), flush=True)
-                DeU = DataExtractor(TheMask,inputfileU,'U')
-                DeV = DataExtractor(TheMask,inputfileV,'V')
+    if p.varname == 'Speed':
+        for frame_index, dt in local_frames:
+            input_file_name_U = 'ave.{}.{}.nc'.format(dt.strftime(dateformat), 'U')
+            input_file_name_V = 'ave.{}.{}.nc'.format(dt.strftime(dateformat), 'V')
+            inputfileU = args.inputdir / input_file_name_U
+            inputfileV = args.inputdir / input_file_name_V
+            datestr = dt.strftime("%d %h %Y - %H:%M UTC")
+            print("rank %d works on %s" %(rank, inputfileU), flush=True)
+            DeU = DataExtractor(TheMask,inputfileU, 'U')
+            DeV = DataExtractor(TheMask,inputfileV, 'V')
 
-                for il, layer in enumerate(LAYERLIST):
-                        layer = Layer(layer.top, layer.top)
-                        U2d = MapBuilder.get_layer_average(DeU, layer)
-                        V2d = MapBuilder.get_layer_average(DeV, layer)
+            for il, layer in enumerate(LAYERLIST):
+                layer = Layer(layer.top, layer.top)
+                U2d = MapBuilder.get_layer_average(DeU, layer)
+                V2d = MapBuilder.get_layer_average(DeV, layer)
 
-                        clim = p.climlist[il] #(0.0, 0.4)
-                        Udict={'varname':'U', 'longname':'Zonal velocity',      'clim':clim, 'layer':layer, 'data':U2d, 'date':datestr,'units':'m s⁻¹'}
-                        Vdict={'varname':'V', 'longname':'Meridional velocity', 'clim':clim, 'layer':layer, 'data':V2d, 'date':datestr,'units':'m s⁻¹'}
-                        fig,ax=map_obj.quiver_plotter_basemap_hourly(Udict, Vdict, TheMask)
-                        outfile = "%save.%02d.%s.%s" % (OUTPUTDIR, ALL_INDEXES[iFrame_plot], p.varname, layer.string())
-                        print(outfile,flush=True)
-                        fig.savefig(outfile + ".png",dpi=86)
-                        pl.close(fig)
+                clim = p.climlist[il] #(0.0, 0.4)
+                Udict={'varname':'U', 'longname':'Zonal velocity',      'clim':clim, 'layer':layer, 'data':U2d, 'date':datestr,'units':'m s⁻¹'}
+                Vdict={'varname':'V', 'longname':'Meridional velocity', 'clim':clim, 'layer':layer, 'data':V2d, 'date':datestr,'units':'m s⁻¹'}
+                fig, ax=map_obj.quiver_plotter_basemap_hourly(Udict, Vdict, TheMask)
+                outfile_name = "ave.{:0>2}.{}.{}.png".format(frame_index, p.varname, layer.string())
+                outfile = args.outputdir / outfile_name
+                print(outfile, flush=True)
+                fig.savefig(outfile, dpi=86)
+                plt.close()
     else:
-
-        for iFrame_plot in local_INDEXES:
-            dt=TL.Timelist[iFrame_plot]
-            inputfile="%save.%s.%s.nc"  %(INPUTDIR,dt.strftime(dateformat),var)
-            datestr=dt.strftime("%d %h %Y - %H:%M UTC")
-            print("rank %d works on %s" %(rank, inputfile), flush=True)
-            De = DataExtractor(TheMask,inputfile,var)
+        for frame_index, dt in local_frames:
+            if args.ignore_before is not None and dt < args.ignore_before:
+                continue
+            inputfile = "ave.{}.{}.nc".format(dt.strftime(dateformat), p.varname)
+            inputfile_path = args.inputdir / inputfile
+            datestr = dt.strftime("%d %h %Y - %H:%M UTC")
+            print("rank %d works on %s" %(rank, inputfile_path), flush=True)
+            De = DataExtractor(TheMask, inputfile_path, p.varname)
 
             for il, layer in enumerate(LAYERLIST):
                 layer = Layer(layer.top, layer.top)
@@ -142,10 +140,9 @@ for p in mb._MapBuilder__plotlist:
                 clim = p.climlist[il]
                 mapdict={'varname':p.varname, 'longname':p.longvarname(), 'clim':clim, 'layer':layer, 'data':map2d, 'date':datestr,'units':p.units()}
                 fig,ax=map_obj.map_plotter_basemap_hourly(mapdict, TheMask)
-                outfile = "%save.%02d.%s.%s" % (OUTPUTDIR, ALL_INDEXES[iFrame_plot], p.varname, layer.string())
-                print(outfile,flush=True)
-                fig.savefig(outfile + ".png",dpi=86)
-                pl.close(fig)
-                #import sys
-                #sys.exit()
+                outfile_name = "ave.{:0>2}.{}.{}.png".format(frame_index, p.varname, layer.string())
+                outfile = args.outputdir / outfile_name
+                print(outfile, flush=True)
+                fig.savefig(outfile, dpi=86)
+                plt.close()
 
